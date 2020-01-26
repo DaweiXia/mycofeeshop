091cdd99062aa1b767e54de4a377297a478c51f0
xiadw <xiadw_public@163.com>
2020-01-26 17:31:24 +0800

    finish delete drinks function

--- backend/src/api.py
+++ backend/src/api.py
@@ -120,8 +120,18 @@ def patch_drinks(payload, id):
 
 @app.route('/drinks/<int:id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
-def delete_drinks(id):
-    pass
+def delete_drinks(payload, id):
+    drink = Drink.query.filter(Drink.id == id).one_or_none()
+    if drink:
+        try:
+            drink.delete()
+            return jsonify({'success': True})
+        except:
+            db.session.rollback()
+            abort(422)
+    else:
+        abort(404)
+
 
 
 # Error Handling
@@ -152,13 +162,13 @@ def unprocessable(error):
 @TODO implement error handler for 404
     error handler should conform to general task above 
 '''
-@app.error_handler(404)
+@app.errorhandler(404)
 def not_found(error):
     return jsonify({
-        "success": False,
-        "error": 404,
-        "message": "resource not found"
-    }), 404
+                    "success": False,
+                    "error": 404,
+                    "message": "resource not found"
+                    }), 404
 
 
 '''
