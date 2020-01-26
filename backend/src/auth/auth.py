42234f9756dcae3707514c31c60fb800e7b1656e
xiadw <xiadw_public@163.com>
2020-01-26 18:12:12 +0800

    delete all todo tips

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -9,13 +9,8 @@ AUTH0_DOMAIN = 'myfsnd.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'coffee'
 
+
 # AuthError Exception
-'''
-AuthError Exception
-A standardized way to communicate auth failure modes
-'''
-
-
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
@@ -23,17 +18,6 @@ class AuthError(Exception):
 
 
 # Auth Header
-
-'''
-@TODO implement get_token_auth_header() method
-    it should attempt to get the header from the request
-        it should raise an AuthError if no header is present
-    it should attempt to split bearer and the token
-        it should raise an AuthError if the header is malformed
-    return the token part of the header
-'''
-
-
 def get_token_auth_header():
     headers = request.headers
     if not headers:
@@ -51,40 +35,12 @@ def get_token_auth_header():
     return header_parts[1]
 
 
-'''
-@TODO implement check_permissions(permission, payload) method
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
-        payload: decoded jwt payload
-
-    it should raise an AuthError if permissions are not included in the payload
-        !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission string is not in the payload permissions array
-    return true otherwise
-'''
-
-
 def check_permissions(permission, payload):
     if 'permissions' not in payload:
         raise AuthError('Permissions not in JWT!', 405)
     if permission not in payload['permissions']:
         raise AuthError('Permission not found!', 405)
     return True
-
-
-'''
-@TODO implement verify_decode_jwt(token) method
-    @INPUTS
-        token: a json web token (string)
-
-    it should be an Auth0 token with key id (kid)
-    it should verify the token using Auth0 /.well-known/jwks.json
-    it should decode the payload from the token
-    it should validate the claims
-    return the decoded payload
-
-    !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
-'''
 
 
 def verify_decode_jwt(token):
@@ -116,18 +72,6 @@ def verify_decode_jwt(token):
             raise AuthError('Invalid header! Unable to parse auth token.', 401)
 
     raise AuthError('Invalid header! Unable to find appropriate key.', 401)
-
-
-'''
-@TODO implement @requires_auth(permission) decorator method
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
-
-    it should use the get_token_auth_header method to get the token
-    it should use the verify_decode_jwt method to decode the jwt
-    it should use the check_permissions method validate claims and check the requested permission
-    return the decorator which passes the decoded payload to the decorated method
-'''
 
 
 def requires_auth(permission=''):
